7f15a7e8c2adbfceb6a35575566a9a27ab41eabe
Lucas Antonio Munoz Albertos <lucasmual93@gmail.com>
2025-01-19 12:24:21 +0100

    ajustando valores de migraciones en database->migrations

--- proyecto_fp/database/migrations/2025_01_09_154717_create_table_calificaciones.php
+++ proyecto_fp/database/migrations/2025_01_09_154717_create_table_calificaciones.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_calificaciones');
+        Schema::dropIfExists('calificaciones');
     }
 };
